f38fa11b840ebba3be530a46e47148509e62314b
mogeku <haomingzidougeixle@gmail.com>
2021-05-29 00:01:14 +0800

    1. 添加TestDemo工程和相关文件结构目录

--- code/src/OpenCvTestDemo.cpp
+++ code/src/OpenCvTestDemo.cpp
@@ -4,19 +4,18 @@
 #include "SampleCommonHead.hpp"
 
 #include <iostream>
-#include <opencv2/opencv.hpp>
 
 
 int main()
 {
-    //TestDisplayImg(u8R"(D:\Project\git\opencv\TestResource\image\0.jpg)");
-    //TestDisplayImgWithBlur(u8R"(D:\Project\git\opencv\TestResource\image\0.jpg)");
-    //TestDisplayImgWithPyrDown(u8R"(D:\Project\git\opencv\TestResource\image\0.jpg)");
-    //TestDisplayImgWithCanny(u8R"(D:\Project\git\opencv\TestResource\image\1.jpg)");
-    //TestChangePixels(u8R"(D:\Project\git\opencv\TestResource\image\1.jpg)");
+    //TestDisplayImg(u8R"(..\..\..\resource\image\1.jpg)");
+    //TestDisplayImgWithBlur(u8R"(..\..\..\resource\image\1.jpg)");
+    //TestDisplayImgWithPyrDown(u8R"(..\..\..\resource\image\1.jpg)");
+    //TestDisplayImgWithCanny(u8R"(..\..\..\resource\image\1.jpg)");
+    //TestChangePixels(u8R"(..\..\..\resource\image\1.jpg)");
 
-    //TestDisplayVideo(u8R"(D:\Project\git\opencv\TestResource\video\0.mp4)");
-    //TestDisplayVideoEx(u8R"(D:\Project\git\opencv\TestResource\video\0.mp4)");
-    TestDisplayCamera();
+    //TestDisplayVideo(u8R"(..\..\..\resource\video\0.mp4)");
+    //TestDisplayVideoEx(u8R"(..\..\..\resource\video\0.mp4)");
+    //TestDisplayCamera();
     std::cout << "Hello World!\n";
 }
